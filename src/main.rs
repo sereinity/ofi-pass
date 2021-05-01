857b481600a971281ebdb4f092cbb4df56b659a5
Sereinity <sereinity@sereinity.fr>
2021-05-02 01:14:55 +0200

    chore: select_secret() now return the Result that is printed by main()

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use std::process::{Command, Stdio};
 extern crate directories;
 use directories::BaseDirs;
+use std::error::Error;
 use std::io::Write;
 
 mod pass;
 
 fn main() {
-    select_secret();
+    println!(
+        "{}",
+        select_secret().expect("Failed to query the desired secret")
+    );
 }
 
-fn select_secret() {
+fn select_secret() -> Result<String, Box<dyn Error>> {
     // List alternative roots and use via PASSWORD_STORE_DIR
     let dir_str = BaseDirs::new().expect("Can't determine directory structure");
     let store_dir = dir_str.home_dir().join(".password-store");
-    // println!("PASSWORD_STORE_DIR: {}", store_dir.to_str().unwrap());
+    // println!("PASSWORD_STORE_DIR: {}", store_dir.to_str()?);
     let store_dir = pass::PassDir::new(store_dir);
 
     let child = Command::new("wofi")
@@ -29,5 +33,5 @@ fn select_secret() {
             .and_then(|mut x| x.write(&(file + "\n").into_bytes()).ok());
     }
     let output = child.wait_with_output().expect("wofi didn't ended well");
-    println!("{}", String::from_utf8(output.stdout).unwrap().trim());
+    Ok(String::from_utf8(output.stdout)?.trim().into())
 }
