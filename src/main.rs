20b64f37377df4869852471dd150edee509b3f5e
Sereinity <sereinity@sereinity.fr>
2021-05-01 23:32:18 +0200

    feature: list entries from default store

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate directories;
+use directories::BaseDirs;
+
+mod pass;
+
 fn main() {
-    println!("Hello, world!");
+    // List alternative roots and use via PASSWORD_STORE_DIR
+    let dir_str = BaseDirs::new().expect("Can't determine directory structure");
+    let store_dir = dir_str.home_dir().join(".password-store");
+    println!("PASSWORD_STORE_DIR: {}", store_dir.to_str().unwrap());
+    let store_dir = pass::PassDir::new(store_dir);
+    for file in store_dir.into_iter() {
+        println!("{}", file);
+    }
 }
---
+++ src/pass.rs
@@ -0,0 +1,38 @@
+use std::path::PathBuf;
+use walkdir::{DirEntry, WalkDir};
+
+pub struct PassDir {
+    pub root: PathBuf,
+}
+
+impl<'a> PassDir {
+    pub fn new(root: PathBuf) -> PassDir {
+        PassDir { root }
+    }
+
+    pub fn into_iter(self: &'a Self) -> impl Iterator<Item = String> + 'a {
+        WalkDir::new(self.root.clone())
+            .min_depth(1)
+            .into_iter()
+            .filter_entry(|x| !is_hidden(x))
+            .filter_map(move |x| clean_name(x.as_ref().unwrap(), &self.root))
+    }
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
+fn clean_name(entry: &DirEntry, prefix: &PathBuf) -> Option<String> {
+    entry
+        .path()
+        .strip_prefix(prefix)
+        .ok()
+        .and_then(|x| x.to_str())
+        .and_then(|x| x.strip_suffix(".gpg"))
+        .and_then(|x| Some(x.to_string()))
+}
