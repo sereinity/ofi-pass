561182a0188196ec5263b10bb1aba0e149856360
Sereinity <sereinity@sereinity.fr>
2021-05-02 19:13:47 +0200

    chore: make select() accept more various Iterators

--- src/wofi.rs
+++ src/wofi.rs
@@ -1,7 +1,11 @@
 use std::io::Write;
 use std::process::{Command, Stdio};
 
-pub fn select(input: impl Iterator<Item = String>) -> Option<String> {
+pub fn select<I, T>(input: I) -> Option<String>
+where
+    I: IntoIterator<Item = T>,
+    T: AsRef<str>,
+{
     let child = Command::new("wofi")
         .args(&["--dmenu"])
         .stdin(Stdio::piped())
@@ -9,10 +13,11 @@ pub fn select(input: impl Iterator<Item = String>) -> Option<String> {
         .spawn()
         .expect("Failed to exec wofi");
     for entry in input {
+        let line = entry.as_ref().to_owned() + "\n";
         child
             .stdin
             .as_ref()
-            .and_then(|mut x| x.write(&(entry + "\n").into_bytes()).ok());
+            .and_then(|mut x| x.write(&(line).into_bytes()).ok());
     }
     let output = child.wait_with_output().expect("wofi didn't ended well");
     String::from_utf8(output.stdout)
