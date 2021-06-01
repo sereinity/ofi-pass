c858e45d3c9a53fbaee33d445d32869b7e487b47
Sereinity <sereinity@sereinity.fr>
2021-06-01 13:28:36 +0200

    fix: increase sleep to 50ms after a Tab

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
                         pass::EType::Field(x) => wtype::wtype(secret.get(&x)),
                         pass::EType::Tab => {
                             wtype::wtype_key("tab");
-                            thread::sleep(time::Duration::from_millis(12));
+                            thread::sleep(time::Duration::from_millis(50));
                         }
                         pass::EType::Enter => wtype::wtype_key("return"),
                         pass::EType::Space => wtype::wtype_key("space"),
